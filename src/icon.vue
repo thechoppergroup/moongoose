26398b9ba2b81a08090821826ceebb6f74c3107a
Adrian Miller <adrianjonmiller@gmail.com>
2018-05-31 11:46:22 -0700

    updated

---
+++ src/icon.vue
@@ -0,0 +1,32 @@
+<style lang="scss">
+    /*
+    span.svg-icon svg {
+        color: #8c8c8c;
+        cursor: pointer;
+
+        &:hover {
+            color: #ffffff;
+        }
+    }
+    */
+</style>
+
+<template>
+  <span class="svg-icon" v-html="rawHtml"></span>
+</template>
+
+<script>
+import Icons from './icons_all'
+
+export default {
+    name: 'moongoose',
+    props: ['name'],
+    data: function() {
+        var out = { rawHtml: '' };
+        return out;
+    },
+    created: function() {
+        this.rawHtml = Icons[this.name];
+    }
+}
+</script>
