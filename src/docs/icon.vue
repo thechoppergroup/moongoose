99a1f64bbf0a530c0b65b09f47765be0adeea055
Adrian Miller <adrianjonmiller@gmail.com>
2018-06-29 10:16:52 -0700

    added junk email and icon vue

---
+++ src/docs/icon.vue
@@ -0,0 +1,59 @@
+<style lang="scss" scoped>
+    button {
+        padding: .5rem;
+        margin: .5rem;
+        display: inline-block;
+        position: relative;
+
+        span {
+            position: absolute;
+            top: calc(100% + .25rem);
+            left: 50%;
+            transform: translate(-50%);
+            opacity: 0;
+            pointer-events: none;
+            padding: .5rem;
+            background: #E7E7E7;
+            box-shadow: 0 2px 2px rgba(black, 0.6);
+        }
+
+        &:hover {
+            background-color:#D8D8D8;
+
+            span {
+                opacity: 1;
+            }
+        }
+    }
+</style>
+
+<template>
+    <button @mouseover="showNow()">
+        <moongoose :name="name" class="a1" v-if="show"></moongoose>
+        <span>{{name}}</span>
+    </button>
+</template>
+
+<script>
+import Moongoose from 'Moongoose/moongoose.vue'
+
+export default {
+    name: 'icon-preview',
+    data: () => {
+        return {
+            show: false
+        }
+    },
+    props: {
+        name: ''
+    },
+    components: {
+        Moongoose
+    },
+    methods: {
+        showNow: function () {
+            this.show = true
+        }
+    }
+}
+</script>
---
+++ src/docs/index.vue
@@ -0,0 +1,84 @@
+<style lang="scss">
+.icons {
+    width: 85%;
+    max-width: 1024px;
+    margin-left: auto;
+    margin-right: auto;
+    font-size: 18px;
+
+    &-list {
+        display: flex;
+        flex-wrap: wrap;
+        
+
+        &-icon {
+            z-index: 1;
+
+            &:hover {
+                z-index: 2;
+            }
+        }
+    }   
+
+    &-search {
+        display: block;
+        width: 100%;
+        font-size: 1em;
+        padding: .25em;
+        outline: none;
+        border-radius: 5px;
+        border: solid 3px #d2d2d2;
+    }
+}
+</style>
+
+<template>
+    <div class="icons">
+        <h1>Moongoose Icons</h1>
+        <input class="icons-search" v-model="filter" placeholder="Search icons..."/>
+        <ul class="icons-list unstyle">
+            <li class="icons-list-icon " v-for="icon in filteredIcons">
+                <icon :name="icon"/>
+            </li >
+        </ul>
+    </div>
+</template>
+
+<script>
+import Icons from 'Moongoose/icons_all.js';
+import Icon from 'Docs/icon.vue';
+import _ from 'lodash';
+
+export default {
+    name: 'preview',
+    el: '#app',
+
+    components: {
+        Icon
+    },
+
+    data: function() {
+        var out = {
+            icons: [],
+            filter: ''
+        };
+        return out;
+    },
+
+    created: function() {
+        this.icons = _.keys(Icons);
+    },
+
+    computed: {
+        filteredIcons: function() {
+            var self = this;
+            return _.filter(_.keys(Icons), function(icon) {
+                return icon.indexOf(self.filter) !== -1;
+            });
+        }
+    },
+
+    methods: {
+    }
+}
+</script>
